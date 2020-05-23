fe54e305200e690571397c3eac0ba6f019c3559b
wolfrayet <yvonne.shenlong@gmail.com>
2020-05-23 20:10:16 +0800

    change normalization region

--- hw04/LF.py
+++ hw04/LF.py
@@ -11,7 +11,7 @@ def phi_L(x,a):
 alpha = [-0.5, -1.0, -1.5]
 x = np.logspace(-2.0,1.0,num=100)
 for a in alpha:
-    phi = 1./integrate.quad(phi_L, 1e-3, np.inf, args=(a))[0]
+    phi = 1./integrate.quad(phi_L, 1e-3, np.inf, args=(a),limit=100)[0]
     L_CDF = phi_L(x,a)*x*phi
     plt.plot(np.log10(x), np.log10(L_CDF), \
             label=r'$\alpha$={:.1f}'.format(a))
